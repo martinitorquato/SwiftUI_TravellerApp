6372a79bf65c8adf6edcf60c6336e7d62d3d8a7e
Raphael <martinitorquato@gmail.com>
2020-07-29 12:08:58 -0300

    Initial Commit

---
+++ TravellerApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TravellerApp
+//
+//  Created by opah on 29/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TravellerApp/TravellerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravellerAppApp.swift
+//  TravellerApp
+//
+//  Created by opah on 29/07/20.
+//
+
+import SwiftUI
+
+@main
+struct TravellerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
